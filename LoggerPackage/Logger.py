c16c16299f4cf987653c1e3eb0ccc53070dbecab
Adriein <adria.claret@gmail.com>
2023-07-02 16:30:37 +0200

    First commit

--- LoggerPackage/Logger.py
+++ LoggerPackage/Logger.py
@@ -7,8 +7,6 @@ class Logger:
         current_date = datetime.now().astimezone().strftime("%d %B %Y").replace(' ', '_')
         log_filename = f"{current_date}_battle_eye_activated.txt"
 
-        print(log_filename)
-
-        with open(log_filename, 'w') as file:
+        with open(log_filename, 'a') as file:
             time_stamp = datetime.now().astimezone().strftime("%d %B %Y, %H:%M:%S")
             file.write(f'[{time_stamp}] {log} \n')
