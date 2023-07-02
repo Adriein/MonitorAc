2d071a867dea9a858fcf8bee5127e97e932c4b1c
Adriein <adria.claret@gmail.com>
2023-07-02 12:39:16 +0200

    First commit

--- MonitorAc.py
+++ MonitorAc.py
@@ -9,17 +9,10 @@ class MonitorAc:
     def start(self) -> None:
         data = [['PID', 'Name']]
 
-        table_data = [
-            ["Name", "Age", "Country"],
-            ["John", 28, "USA"],
-            ["Emily", 32, "Canada"],
-            ["Michael", 45, "UK"],
-        ]
-
         for process in self.active_processes.values:
             data.append([process.pid, process.name])
 
-        Table.create(table_data).print()
+        Table.create(data).print()
 
 
 MonitorAc().start()
