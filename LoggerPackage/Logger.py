1ee27899a786c44af99524d4389cf72cf2a1799f
Adriein <adria.claret@gmail.com>
2023-07-02 16:27:29 +0200

    First commit

--- LoggerPackage/Logger.py
+++ LoggerPackage/Logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Logger:
     @staticmethod
     def write_in_disk(log: str) -> None:
-        current_date = datetime.now().astimezone().strftime("%d %B %Y")
+        current_date = datetime.now().astimezone().strftime("%d %B %Y").replace(' ', '_')
         log_filename = f"{current_date}_battle_eye_activated.txt"
 
         print(log_filename)
