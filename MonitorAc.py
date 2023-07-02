8658c45ce41665a3d9009c3376b79abf4294aae0
Adriein <adria.claret@gmail.com>
2023-07-02 16:23:58 +0200

    First commit

--- MonitorAc.py
+++ MonitorAc.py
@@ -8,15 +8,19 @@ class MonitorAc:
 
     def watch(self) -> None:
         try:
+            print('Start monitoring battle eye activity')
+
             while True:
                 for process in self.active_processes.values:
                     if 'BE' in process.name or 'tibia' in process.name:
                         Logger.write_in_disk(f'Process(pid={process.pid}, name={process.name})')
 
         except KeyboardInterrupt:
+            print('Graceful shutdown')
             Logger.write_in_disk('Graceful shutdown')
             raise SystemExit
         except Exception as error:
+            print(str(error))
             Logger.write_in_disk(str(error))
             raise SystemExit from error
 
