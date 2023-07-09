3f83a6b862d276d3b548cda5431ef1db88d5f104
Adriein <adria.claret@gmail.com>
2023-07-09 16:16:59 +0200

    trying to find child process by pid

--- ProcessPackage/Process.py
+++ ProcessPackage/Process.py
@@ -38,6 +38,9 @@ class Process:
 
                     parent_process_id = int(stat_fields[ppid_position])
 
+                    print(f'Process watched pid {stat_fields[0]} parent pid {parent_process_id}')
+                    print(parent_process_id == int(self.pid))
+
                     if parent_process_id == int(self.pid):
                         print(stat_fields)
                         child_processes.append(Process(str(child_pid), ''))
